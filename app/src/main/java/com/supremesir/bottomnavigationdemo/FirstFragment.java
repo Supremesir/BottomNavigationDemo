60b464f434f1a4bb4215f9d8bf934a6edc08c3ad
Haofan Fang <haofanfang@gmail.com>
2020-04-09 17:59:28 +0800

    update FirstFragment click to rotate image

--- app/src/main/java/com/supremesir/bottomnavigationdemo/FirstFragment.java
+++ app/src/main/java/com/supremesir/bottomnavigationdemo/FirstFragment.java
@@ -1,7 +1,9 @@
 package com.supremesir.bottomnavigationdemo;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,9 +14,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.supremesir.bottomnavigationdemo.databinding.FirstFragmentBinding;
+
 public class FirstFragment extends Fragment {
 
     private FirstViewModel mViewModel;
+    private FirstFragmentBinding binding;
 
     public static FirstFragment newInstance() {
         return new FirstFragment();
@@ -23,14 +28,25 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.first_fragment, container, false);
+        binding = FirstFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+//        return inflater.inflate(R.layout.first_fragment, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(FirstViewModel.class);
-        // TODO: Use the ViewModel
+        mViewModel = new ViewModelProvider(requireActivity()).get(FirstViewModel.class);
+        final ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(binding.imageView, "rotation", 0, 0);
+        objectAnimator.setDuration(500);
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                objectAnimator.setFloatValues(binding.imageView.getRotation(), binding.imageView.getRotation() + 90);
+                objectAnimator.start();
+            }
+        });
+//        mViewModel = ViewModelProviders.of(this).get(FirstViewModel.class);
     }
 
 }
